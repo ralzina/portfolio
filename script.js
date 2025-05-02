db5688d60a015a7a5361c5ecf77a8ce6b197d3cd
Rene Alzina <renealzina@mac.dhcp.nd.edu>
2025-05-02 01:02:15 -0400

    Added contact card

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const themeToggle = document.getElementById("theme-toggle");
 const themeIcon = document.getElementById("theme-icon");
 const giticon = document.getElementById("giticon")
 const linkicon = document.getElementById("linkicon")
+const giticon_footer = document.getElementById("giticon-footer")
+const linkicon_footer = document.getElementById("linkicon-footer")
 
 document.body.classList.add("dark-mode");  
 
@@ -12,10 +14,15 @@ themeToggle.addEventListener("click", () => {
     themeIcon.src = "images/sun-icon.svg";  
     giticon.src = "images/github-light.svg";  
     linkicon.src = "images/linkedin-light.svg";  
+    giticon_footer.src = "images/github-light.svg";  
+    linkicon_footer.src = "images/linkedin-light.svg";  
   } else {
     themeIcon.src = "images/moon-icon.svg";  
     
     giticon.src = "images/github_icon.svg"; 
     linkicon.src = "images/linkedin_icon.svg";  
+    giticon_footer.src = "images/github_icon.svg"; 
+    linkicon_footer.src = "images/linkedin_icon.svg"; 
+    
   }
 });
