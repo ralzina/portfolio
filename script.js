aa133ac88bcbdfe03999e9951c3bada0c72e319a
Rene Alzina <renealzina@Renes-MacBook-Pro.local>
2025-07-28 13:28:13 -0700

    Swapped light and dark themes and adjusted project card height

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ const linkicon = document.getElementById("linkicon")
 const giticon_footer = document.getElementById("giticon-footer")
 const linkicon_footer = document.getElementById("linkicon-footer")
 
-document.body.classList.add("dark-mode");  
 
 themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
 
-  if (document.body.classList.contains("dark-mode")) {
+  if (document.body.classList.contains("light-mode")) {
+    themeIcon.src = "images/moon-icon.svg";  
+    giticon.src = "images/github_icon.svg"; 
+    linkicon.src = "images/linkedin_icon.svg";  
+    giticon_footer.src = "images/github_icon.svg"; 
+    linkicon_footer.src = "images/linkedin_icon.svg"; 
+  } else {
     themeIcon.src = "images/sun-icon.svg";  
     giticon.src = "images/github-light.svg";  
     linkicon.src = "images/linkedin-light.svg";  
     giticon_footer.src = "images/github-light.svg";  
     linkicon_footer.src = "images/linkedin-light.svg";  
-  } else {
-    themeIcon.src = "images/moon-icon.svg";  
     
-    giticon.src = "images/github_icon.svg"; 
-    linkicon.src = "images/linkedin_icon.svg";  
-    giticon_footer.src = "images/github_icon.svg"; 
-    linkicon_footer.src = "images/linkedin_icon.svg"; 
     
   }
 });
